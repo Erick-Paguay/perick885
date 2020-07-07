2e537da039d0448b8d84fb66d6efe8103962550f
Erick-Paguay <66690902+Erick-Paguay@users.noreply.github.com>
2020-07-07 12:37:30 -0500

    Add files via upload

---
+++ import 1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 11:24:12 2020
+
+@author: SEDMQ
+"""
+
+
+import math 
+print(math.sin(math.pi/2))
---
+++ import-mathv2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 12:16:35 2020
+
+@author: SEDMQ
+"""
+
+import math 
+def sin(x):
+    if 2*x==pi:
+        return 0.99999999
+    else:
+        return None
+pi=3.14
+print(sin(pi/2))
+print(math.sin(math.pi/2))
