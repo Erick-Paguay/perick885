67f35a042f4a577a94dc31956935ac32433d8139
Erick-Paguay <66690902+Erick-Paguay@users.noreply.github.com>
2020-07-10 12:13:34 -0500

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 10 12:07:17 2020
+
+@author: SEDMQ
+"""
+
+
+import numpy as np
+array = np.array([[1,2,3,4], [5,6,7,8,9,10]], dtype = np.int64)
+print (array)
